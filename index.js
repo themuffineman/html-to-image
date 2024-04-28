cda36a384c07b9b9ee535179eb27288d57817a5c
Petrus <petrusheya@gmail.com>
2024-04-28 22:48:13 +0200

    Server setup

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ const app = express()
 
 app.listen(PORT, ()=> {console.log(`Server listening on port ${PORT}`)})
 
-app.post('/screenshot', (req , res)=>{
+app.get('/screenshot', async (req , res)=>{
     const {name, service, location} = req.body
     console.log(`Name: ${name}, Service: ${service}, Location: ${location}`)
+    const html = `helow`
+    const browser = await puppeteer.launch();
+    const page = browser.newPage()
+    page.setDefaultNaviagtionTimout(100000)
+    page.setContent(html)
+    const screeenshot = page.screenshot({ encoding: 'base64' })
+
+    res.json({src: screenshot})
+    
 })
